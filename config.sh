76337b95e29b178847c2f3cb6f2dfcc7a9709488
Markus Doering <markus.doering@sap.com>
2016-02-03 16:51:52 +0100

    added needed quotes (2)

--- config.sh
+++ config.sh
@@ -12,9 +12,9 @@ A_SHELL=/usr/bin/fish
 A_LOCALE="en_US.UTF-8 UTF-8"
 
 A_PACKAGE_LIST="python python2 git gcc sudo grub openssh xorg-server"
-A_PACKAGE_LIST=$A_PACKAGE_LIST "vim fish thefuck bash-completion"
-A_PACKAGE_LIST=$A_PACKAGE_LIST "xmonad xmonad-contrib xmobar dmenu lightdm lilyterm"
-A_PACKAGE_LIST=$A_PACKAGE_LIST "jdk8-openjdk maven"
-A_PACKAGE_LIST=$A_PACKAGE_LIST "ttf-dejavu ttf-freefont ttf-oxygen"
+A_PACKAGE_LIST="$A_PACKAGE_LIST vim fish thefuck bash-completion"
+A_PACKAGE_LIST="$A_PACKAGE_LIST xmonad xmonad-contrib xmobar dmenu lightdm lilyterm"
+A_PACKAGE_LIST="$A_PACKAGE_LIST jdk8-openjdk maven"
+A_PACKAGE_LIST="$A_PACKAGE_LIST ttf-dejavu ttf-freefont ttf-oxygen"
 
 
